28f27e1ad6cefde20581637bc06625fad887e61a
Will Storey <will@summercat.com>
2011-07-06 14:46:17 -0700

    Launch skeletester with stage. We can see occupancy grid in RViz, but skeleplanner currently crashes upon initialize.

--- skeleplanner/src/skeletester.cpp
+++ skeleplanner/src/skeletester.cpp
@@ -122,8 +122,12 @@ int main(int argc, char** argv) {
   tf::TransformListener tf_(ros::Duration(10.0));
   costmap_2d::Costmap2DROS* costmap_ = new costmap_2d::Costmap2DROS(std::string("skeletester_costmap"), tf_);
 
-  //SkelePlanner skele_planner;
-  //skele_planner.initialize("skeletester", costmap_);
+/*
+  ROS_WARN("Initialising SkelePlanner object...");
+  SkelePlanner skele_planner;
+  skele_planner.initialize("skeletester", costmap_);
+  ROS_WARN("SkelePlanner object initialised.");
+*/
 
   marker_id = 0;
 
@@ -134,10 +138,10 @@ int main(int argc, char** argv) {
     ROS_WARN("Skeletester publishing...");
 
     // First publish the current occupancy grid
-    //publish_map(costmap_, &map_pub);
+    publish_map(costmap_, &map_pub);
 
+/*
     // Markers for each node in the topological map
-    /*
     std::vector<visualization_msgs::Marker> markers;
     for (std::vector<Waypoint*>::const_iterator it = skele_planner.topomap->begin();
       it != skele_planner.topomap->end(); ++it)
@@ -165,10 +169,10 @@ int main(int argc, char** argv) {
     {
       marker_pub.publish( *it );
     }
+*/
 
     // Additionally we can publish chosen path
     // makePlan() then add nodes & edges with different colours
-    */
 
     ros::spinOnce();
     loop_rate.sleep();
