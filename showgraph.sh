a5272ce65c2a609af8b7b67936863c15f2e42bb2
Peter N <pwn4@sfu.ca>
2011-08-15 11:44:33 -0700

    modified showgraph to use the processed data so that graphs are vs. distance travelled instead of time. Results look good!

--- showgraph.sh
+++ showgraph.sh
@@ -3,13 +3,14 @@
 #              dashed defaultplex "Helvetica" 14
 # set output 'output.ps'
 
-RUN=5
+RUN=8
 MAP="hospital"
+SUFFIX=".processed"
 
-set xlabel "Time" 0.000000,0.000000
+set xlabel "Distance Travelled" 0.000000,0.000000
 set ylabel "Maximum Distance Discovered" 0.000000,0.000000
 set title "Robot Performance" 0.000000,0.000000
 
-plot "data/".MAP."/".RUN."-0-".MAP using 1:4 title "Run ".RUN.", A" with lines, \
-    "data/".MAP."/".RUN."-1-".MAP using 1:4 title "Run ".RUN.", B" with lines, \
-    "data/".MAP."/".RUN."-2-".MAP using 1:4 title "Run ".RUN.", C" with lines
+plot "data/".MAP."/".RUN."-0-".MAP.SUFFIX using 1:2 title "Run ".RUN.", A" with lines, \
+    "data/".MAP."/".RUN."-1-".MAP.SUFFIX using 1:2 title "Run ".RUN.", B" with lines, \
+    "data/".MAP."/".RUN."-2-".MAP.SUFFIX using 1:2 title "Run ".RUN.", C" with lines
