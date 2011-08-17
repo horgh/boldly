c707445494976d206ceb18cb7ced169e057e4a4b
Peter N <pwn4@sfu.ca>
2011-08-17 10:43:58 -0700

    Modified / improved stats testing. I first setup a 95% confidence interval for file2, and then do a hypothesis test on the lower bound of that interval against the supposed mean of file1. If the test passes, then 95% of the time file1's mean is significantly lower.

--- data/analyze.cpp
+++ data/analyze.cpp
@@ -7,85 +7,98 @@
 
 using namespace std;
 
+struct Statistic{
+    double mean;
+    double stddev;
+    double n;
+    double confl;
+    double confr;
+    vector<double> values;
+};
 
+Statistic getStatistics(char* filename)
+{
+    FILE * input;
+    input = fopen(filename, "r");
+    
+    Statistic rtn;
+    
+    double a = 0, b = 0;
+    while(fscanf(input, "%lf %lf", &a, &b) != EOF)
+    {
+        rtn.n++;
+        rtn.mean += a;
+        rtn.values.push_back(a);
+    }
+    rtn.mean /= rtn.n;
+    
+    //calc std dev
+    for(vector<double>::iterator it = rtn.values.begin(); it != rtn.values.end(); it++)
+        rtn.stddev += ((*it)-rtn.mean)*((*it)-rtn.mean);
+    
+    rtn.stddev = sqrt( rtn.stddev / (rtn.n - 1) );
+    
+    //calc 95% confidence interval
+    double tValue = 1.833;
+    rtn.confl = rtn.mean - (tValue * (rtn.stddev / sqrt(rtn.n)));
+    rtn.confr = rtn.mean + (tValue * (rtn.stddev / sqrt(rtn.n)));
+    
+    return rtn;
+}
 
 int main(int argc, char **argv)
 {
     if(argc >= 2)
     {
         //read input
-        FILE * input;
-        input = fopen(argv[1], "r");
+        Statistic file1 = getStatistics(argv[1]);
+        Statistic file2;
         
-        double mean2 = 0;
-        int n2 = -1;
         if(argc >= 3)
         {
-            n2 = 0;
-            FILE * input2;
-            input2 = fopen(argv[2], "r");
-            
-            double a = 0, b = 0;
-            while(fscanf(input2, "%lf %lf", &a, &b) != EOF)
-            {
-                n2++;
-                mean2 += a;
-            }
-            mean2 /= n2;
+            file2 = getStatistics(argv[2]);
         }
-        
-        vector<double> data;
-        double mean = 0;
-        double stddev = 0;
-        double n = 0;
-        
-        double a = 0, b = 0;
-        while(fscanf(input, "%lf %lf", &a, &b) != EOF)
+ 
+        printf("--------------- File 1 Statistics ---------------\n");
+        printf("Data: ");
+        for(vector<double>::iterator it = file1.values.begin(); it != file1.values.end(); it++)
+            printf("%lf ", (*it));
+        printf("\nMean: %lf\n", file1.mean);
+        printf("Std Dev: %lf\n", file1.stddev);
+        printf("Variance: %lf\n", file1.stddev*file1.stddev);
+        printf("95%% Confidence Interval: (%lf, %lf)\n", file1.confl, file1.confr);
+        printf("-------------------------------------------------\n");
+        if(argc >= 3)
         {
-            n++;
-            mean += a;
-            data.push_back(a);
+            printf("--------------- File 2 Statistics ---------------\n");
+            printf("Data: ");
+            for(vector<double>::iterator it = file2.values.begin(); it != file2.values.end(); it++)
+                printf("%lf ", (*it));
+            printf("\nMean: %lf\n", file2.mean);
+            printf("Std Dev: %lf\n", file2.stddev);
+            printf("Variance: %lf\n", file2.stddev*file2.stddev);
+            printf("95%% Confidence Interval: (%lf, %lf)\n", file2.confl, file2.confr);
+            printf("-------------------------------------------------\n");
         }
-        mean /= n;
-        
-        //calc std dev
-        for(vector<double>::iterator it = data.begin(); it != data.end(); it++)
-            stddev += ((*it)-mean)*((*it)-mean);
-        
-        stddev = sqrt( stddev / (n - 1) );
-        
-        //calc 95% confidence interval
-        double confl = mean - (1.96 * (stddev / n));
-        double confr = mean + (1.96 * (stddev / n));
         
         //assuming normal dist, use p.301 of Devore "Probability and Statistics" - using the null distribution
         double alpha = 0.05;
         int degreesOfFreedom = 9;
         double tReject = -1.833; //pregenerated from book's appendix
-        double t = (mean - mean2) / (stddev / sqrt(n));
-        
-        //output
-        printf("Data: ");
-        for(vector<double>::iterator it = data.begin(); it != data.end(); it++)
-            printf("%lf ", (*it));
-        printf("\nMean: %lf\n", mean);
-        printf("Std Dev: %lf\n", stddev);
-        printf("Variance: %lf\n", stddev*stddev);
+        //we are testing against the left side of the confidence interval for file2's mean
+        double t = (file1.mean - file2.confl) / (file1.stddev / sqrt(file1.n));
+
         
         if(argc >= 3)
         {
-            printf("H0: mean(file1) = mean(file2)\n");
-            printf("Ha: mean(file1) < mean(file2)\n", alpha);
+            printf("H0: mean(file1) = mean(file2) (left side of 95%% conf int. )\n");
+            printf("Ha: mean(file1) < mean(file2) (the lowest 'plausible' mean)\n", alpha);
             printf("Sig Level: %lf\n", alpha);
             printf("Degress of Freedom: %d\n", degreesOfFreedom);
             printf("criteria for rejection of H0: t <= %lf\n", tReject);
             printf("t: %lf\n", t);
-            printf("Conclusion: %s\n", (t <= tReject? "Reject the null hypothesis." : "Do not reject the null hypothesis."));
-        }
-        
-        //we don't know the population stddev, only the sample. So, we can't do CIs
-        //printf("95%% Confidence Interval: (%lf, %lf)\n", confl, confr);
-        
+            printf("Conclusion: %s\n", (t <= tReject? "Reject the null hypothesis. 95% of the time, file1's mean is lower with 0.05 statistical significance." : "Do not reject the null hypothesis."));
+        }        
     
         return 0;
     
